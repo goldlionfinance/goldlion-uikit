dfb07bd5b295993142f63b8753e5a52c47b44348
Literally Collective <bluedragon.cstar@outlook.com>
2021-06-08 18:32:42 -0300

    Feat: update card color

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2818,7 +2818,7 @@ var brandColors = {
 var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#452A7A", textDisabled: "#BDC2C4", textSubtle: "#3a3101", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#3e3718", background: "#10242b", backgroundDisabled: "#333a3e", contrast: "#FFFFFF", invertedContrast: "#25210d", input: "#25210d", primaryDark: "##0d0e0e", tertiary: "#2f2803", text: "#e8aa2d", textDisabled: "#666171", textSubtle: "#ddd1c1", borderColor: "#524B63", card: "#391d07c9", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#3e3718", background: "#10242b", backgroundDisabled: "#333a3e", contrast: "#FFFFFF", invertedContrast: "#25210d", input: "#25210d", primaryDark: "##0d0e0e", tertiary: "#2f2803", text: "#e8aa2d", textDisabled: "#666171", textSubtle: "#ddd1c1", borderColor: "#524B63", card: "#4a3b20c9", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
